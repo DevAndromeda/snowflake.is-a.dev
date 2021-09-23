4152b278803e1c6717ddef9f10237173759d2700
Andromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-09-23 20:30:08 +0545

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,15 +6,15 @@ export default function App({ Component, pageProps }) {
             <Head>
                 <meta name="twitter:card" content="summary" />
                 <meta name="twitter:url" content="https://snowflake.is-a.dev" />
-                <meta name="twitter:title" content="Snowflake" />
-                <meta name="twitter:description" content="Hello, my name is Snowflake." />
+                <meta name="twitter:title" content="and" />
+                <meta name="twitter:description" content="Hello, my name is and." />
                 <meta name="twitter:image" content="https://snowflake.is-a.dev/logo.png" />
-                <meta name="twitter:creator" content="@DevSnowflake" />
+                <meta name="twitter:creator" content="@DevAndromeda7" />
 
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Snowflake" />
-                <meta property="og:description" content="Hello, my name is Snowflake." />
-                <meta property="og:site_name" content="SnowflakeX" />
+                <meta property="og:description" content="Hello, my name is and." />
+                <meta property="og:site_name" content="and" />
                 <meta property="og:url" content="https://snowflake.is-a.dev" />
                 <meta property="og:image" content="https://snowflake.is-a.dev/logo.png" />
 
@@ -28,7 +28,6 @@ export default function App({ Component, pageProps }) {
                 {/* JS */}
                 <script src="/scripts/jquery.min.js"></script>
                 <script src="/scripts/bootstrap.min.js"></script>
-                {/* <script src="/scripts/script.js" defer></script> */}
             </Head>
 
             <Component {...pageProps} />
